0a6fd1087ccef7565b258da9a791d68536b90b87
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-18 21:46:01 +0900

    스프링 데이터 Common 8. 기본 리포지토리 커스터마이징

--- src/main/java/me/whiteship/demojpa3/post/PostRepository.java
+++ src/main/java/me/whiteship/demojpa3/post/PostRepository.java
@@ -1,6 +1,7 @@
 package me.whiteship.demojpa3.post;
 
+import me.whiteship.demojpa3.MyRepository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Post, Long>, PostCustomRepository<Post> {
+public interface PostRepository extends MyRepository<Post, Long>, PostCustomRepository<Post> {
 }
--- src/test/java/me/whiteship/demojpa3/post/PostRepositoryTest.java
+++ src/test/java/me/whiteship/demojpa3/post/PostRepositoryTest.java
@@ -6,6 +6,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 @RunWith(SpringRunner.class)
 @DataJpaTest
 public class PostRepositoryTest {
@@ -16,7 +18,12 @@ public class PostRepositoryTest {
     public void curd() {
         Post post = new Post();
         post.setTitle("hibernate");
+
+        assertThat(postRepository.contains(post)).isFalse();
+
         postRepository.save(post);
+
+        assertThat(postRepository.contains(post)).isTrue();
 
         postRepository.findMyPost();
 
