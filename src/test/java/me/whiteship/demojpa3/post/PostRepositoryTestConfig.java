a6bda66aebb6846413c4dc604bbdd884275081a7
DESKTOP-D3F5JN6\sejong <sejongdeveloper@gmail.com>
2023-01-18 22:22:22 +0900

    스프링 데이터 Common 9. 도메인 이벤트

--- src/test/java/me/whiteship/demojpa3/post/PostRepositoryTestConfig.java
+++ src/test/java/me/whiteship/demojpa3/post/PostRepositoryTestConfig.java
@@ -1,5 +1,6 @@
 package me.whiteship.demojpa3.post;
 
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -7,7 +8,11 @@ import org.springframework.context.annotation.Configuration;
 public class PostRepositoryTestConfig {
 
     @Bean
-    public PostListener postListener() {
-        return new PostListener();
+    public ApplicationListener<PostPublishedEvent> postListener() {
+        return postPublishedEvent -> {
+            System.out.println("----------------");
+            System.out.println(postPublishedEvent.getPost().getTitle() + " is published!!");
+            System.out.println("----------------");
+        };
     }
 }
